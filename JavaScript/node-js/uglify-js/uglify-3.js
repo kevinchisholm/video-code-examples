2947b8f30fbc67fd8479930c2f1bad5d4a1288ba
Kevin Chisholm <kevin@kevinchisholm.com>
2015-01-25 16:03:30 -0500

    finish up uglify-js example

---
+++ JavaScript/node-js/uglify-js/uglify-3.js
@@ -0,0 +1,19 @@
+//get a reference to the file system module
+var fs = require('fs');
+
+//get a reference to the uglify-js module
+var UglifyJS = require('uglify-js');
+
+//get a reference to the minified version of file-1.js, file-2.js and file-3.js
+var result = UglifyJS.minify(["file-1.js", "file-2.js", "file-3.js"]);
+
+//view the output
+console.log(result.code);
+
+fs.writeFile("output.min.js", result.code, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("File was successfully saved.");
+    }
+});
