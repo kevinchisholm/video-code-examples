40cbe2cb0c546a9b2409bfaeeee5c06f2079fdcb
Kevin Chisholm <kevin@kevinchisholm.com>
2015-01-27 19:19:13 -0500

    add code clean up and strict mode

--- JavaScript/node-js/node-modules/fs/fs-rename/rename-1.js
+++ JavaScript/node-js/node-modules/fs/fs-rename/rename-1.js
@@ -1,36 +1,24 @@
+"use strict";
 //set a reference to the file system module
-var fs = require('fs');
-	//set a reference to the filepath module
-	FP = require('filepath'),
-	//set a reference to the old file name
-	oldFileName = 're-name-me.txt',
-	//set a reference to the new file name
-	newFileName = 'ive-been-renamed.txt',
-	//set a reference to the folder structure that leads up to the current file, add a trailing slash
-	fpFolder = FP.newPath() + '/',
-	//set a reference to the old file
-	fpFile = FP.newPath(oldFileName),
-	//set a reference to the old file path
-	//oldFilePath = fpFolder + fpFile,
-	oldFilePath = fpFolder + (fs.existsSync(fpFolder + fpFile) ? oldFileName  : newFileName);
-
-	//set a reference to the new file path
-	newFilePath = fpFolder + (fs.existsSync(fpFolder + fpFile) ? newFileName  : oldFileName);
-
-
-//console.log('fs.exists: ' + fs.existsSync(oldFilePath))
-
-//console.log('newFilePath: ' + newFilePath);
-
-//console.log('fpFolder: ' + fpFolder);
-
-//console.log('oldFilePath: ' + oldFilePath);
-
-//console.log('newFilePath: ' + newFilePath);
+var fs = require('fs'),
+  //set a reference to the filepath module
+  FP = require('filepath'),
+  //set a reference to the old file name
+  oldFileName = 're-name-me.txt',
+  //set a reference to the new file name
+  newFileName = 'ive-been-renamed.txt',
+  //set a reference to the folder structure that leads up to the current file, add a trailing slash
+  fpFolder = FP.newPath() + '/',
+  //set a reference to the old file
+  fpFile = FP.newPath(oldFileName),
+  //set a reference to the old file path
+  oldFilePath = fpFolder + (fs.existsSync(fpFolder + fpFile) ? oldFileName  : newFileName),
+  //set a reference to the new file path
+  newFilePath = fpFolder + (fs.existsSync(fpFolder + fpFile) ? newFileName  : oldFileName);
 
 //use the fs object's rename method to re-name the file
-fs.rename(oldFilePath, newFilePath, function(err){
-	if (err){console.log(err); return; }
+fs.rename(oldFilePath, newFilePath, function (err) {
+  if (err) {console.log(err); return; }
 
-	console.log('The file has been re-named to: ' + newFilePath);
-})
+  console.log('The file has been re-named to: ' + newFilePath);
+});
