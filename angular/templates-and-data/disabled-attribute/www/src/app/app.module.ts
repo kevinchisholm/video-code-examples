5e68f8fadc76c4b7bdb58c0e5d0cf06de42c098e
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2017-10-28 16:41:09 -0400

    Add templates-and-data/disabled-attribute

---
+++ angular/templates-and-data/disabled-attribute/www/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserModule} from '@angular/platform-browser';
+import {Http, HttpModule, BaseRequestOptions} from '@angular/http';
+import {AppComponent} from './app.component';
+import {HomeComponent} from '../home/home.component';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import {routing} from './routes';
+
+@NgModule({
+  providers: [
+    BaseRequestOptions,
+    HttpModule,
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
+  ],
+  imports: [BrowserModule, HttpModule, routing],
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
