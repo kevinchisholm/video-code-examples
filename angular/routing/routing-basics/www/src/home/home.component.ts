34cc165219c7bacf7f428b4910c697455380bc98
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2017-10-28 16:29:31 -0400

    Add angular/routing

---
+++ angular/routing/routing-basics/www/src/home/home.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: 'src/home/home.html'
+})
+export class HomeComponent {
+  constructor() {   
+
+  }
+
+  public ngOnInit () {
+
+  }
+}
