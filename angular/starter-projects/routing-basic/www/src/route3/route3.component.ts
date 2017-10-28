fd7a212b7ff7608caccccbdad8726d70a9607bae
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2017-10-28 16:34:41 -0400

    Add starter-projects/routing-basic

---
+++ angular/starter-projects/routing-basic/www/src/route3/route3.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'route3',
+  templateUrl: 'src/route3/route3.html'
+})
+export class Route3Component {
+  constructor() {   
+
+  }
+
+  public ngOnInit () {
+
+  }
+}
