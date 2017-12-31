21a0d6734b59c258be3c4becb87d947282907137
Kevin Chisholm <kevin212@pobox.com>
2017-12-31 17:04:25 -0500

    Remove un-needed stuff from ParentComponent

--- angular/components/@input/www/src/parent/parent.component.ts
+++ angular/components/@input/www/src/parent/parent.component.ts
@@ -1,19 +1,10 @@
-import { Component, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'parent',
   templateUrl: 'src/parent/parent.html'
 })
 export class ParentComponent {
-
   public userName = '';
   public userPhone = '';
-
-  constructor() {   
-
-  }
-
-  public ngOnInit () {
-
-  }
 }
