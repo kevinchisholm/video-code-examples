bb8d48e3f5c5b9d7d3e2f40616844595b73f2c10
Kevin Chisholm <kevin212@pobox.com>
2018-03-04 07:11:53 -0500

    Code cleanup

--- node/file-uploads-with-multer/index.js
+++ node/file-uploads-with-multer/index.js
@@ -1,23 +1,26 @@
-const express = require('express')
+const serverPort = 3000,
+    express = require('express'),
+    multer = require('multer'),
+    upload = multer({dest: 'uploads/'}),
+    app = express();
 
-const app = express()
-
-const multer = require('multer')
-
-const upload = multer({dest: 'uploads/'})
-
+//set the view engine
 app.set('view engine', 'ejs')
 
+//add a handler for the GET / route
 app.get('/', (req, res) => res.render('home'))
 
+//add a handler for the POST / route
 app.post('/', upload.single('img'), (req, res) => {
-
+    //for demonstration purposes only:
     console.log(req.body);
-
     console.log(req.file);
 
-res.send('ok')
-
+    //send a response to the client
+    res.send('<h2>File upload succeeded.</h2><a href="/">Upload another file<a>');
 })
 
-app.listen(3000, () => console.log('running OK B...'))
+//start the web server
+app.listen(serverPort, () => {
+    console.log('server running on http://localhost:' + serverPort);
+})
